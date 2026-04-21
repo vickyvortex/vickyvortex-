89b3f82e97c8f9ea5270280352325d4384571e98
Vicky Vortex <restore@dignityclinic.co.uk>
2026-04-22 00:14:51 +0100

    fix: disable Next.js image optimization to resolve broken images

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   images: {
     minimumCacheTTL: 60,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    dangerouslyAllowSVG: false,
+    unoptimized: true,
   },
 }
 

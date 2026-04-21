9b83d8202e04ac883ef3bab435a89cdd7d810852
Vicky Vortex <restore@dignityclinic.co.uk>
2026-04-22 00:14:36 +0100

    fix: increase Next.js image size limits

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    minimumCacheTTL: 60,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    dangerouslyAllowSVG: false,
+  },
+}
 
 module.exports = nextConfig
